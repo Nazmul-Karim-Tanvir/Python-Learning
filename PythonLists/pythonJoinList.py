e6c3e2a274dc31eae7ab48bca7b7835a4dbf2ea0
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-11-22 16:08:30 +0600

    practicing tuple

---
+++ PythonLists/pythonJoinList.py
@@ -0,0 +1,24 @@
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+
+#  Using append
+
+list = [1, 2, 3]
+list2 = ["10","tanvir",["10",30]]
+
+for  x in list2:
+    list1.append(x)
+    
+print(list1)
+
+# using extend
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
---
+++ PythonTuples/tuples.py
@@ -0,0 +1,18 @@
+mytuple = ("apple", "banana", "cherry")
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+print(type(thistuple))

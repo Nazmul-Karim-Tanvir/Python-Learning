b88fc0a27aa82fc60c7cd2d0f34d7c0cb06a47a5
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-11-22 20:05:01 +0600

    tuple access practice done

--- PythonTuples/accessTuple.py
+++ PythonTuples/accessTuple.py
@@ -16,3 +16,8 @@ print(thistuple[:4])
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[2:])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi")
+print(thistuple[:4])
+
+
